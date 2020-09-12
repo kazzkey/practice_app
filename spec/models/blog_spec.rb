821543754cb042078d7bdff9f766b182a50e5d13
Kazuki Kawano <k_kawano86@yahoo.co.jp>
2020-09-12 23:06:27 +0900

    add model spec

--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -1,5 +1,52 @@
 require 'rails_helper'
 
 RSpec.describe Blog, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'タイトルと内容が入力されている場合、有効である' do
+    blog = Blog.new(
+      title: 'Test',
+      content: 'Hello World. This is my blog...',
+    )
+    expect(blog).to be_valid
+  end
+
+  it 'タイトルがない場合、無効である' do
+    blog = Blog.new(
+      title: nil,
+      content: 'Hello World. This is my blog...',
+    )
+    expect(blog).to be_invalid
+  end
+
+  it '内容がない場合、無効である' do
+    blog = Blog.new(
+      title: 'Test',
+      content: nil,
+    )
+    expect(blog).to be_invalid
+  end
+
+  it '文字数制限が守られている場合、有効である' do
+    blog = Blog.new(
+      title: 'a' * 25,
+      content: 'a' * 100,
+    )
+    expect(blog).to be_valid
+  end
+
+  it 'タイトルが25字以上だった場合、無効である' do
+    blog = Blog.new(
+      title: 'a' * 26,
+      content: 'Hello World. This is my blog...',
+    )
+    expect(blog).to be_invalid
+  end
+
+  it '内容が100字以上だった場合、無効である' do
+    blog = Blog.new(
+      title: 'Test',
+      content: 'a' * 101,
+    )
+    expect(blog).to be_invalid
+  end
 end
